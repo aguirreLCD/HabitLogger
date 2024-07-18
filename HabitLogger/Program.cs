136500fd70b9aebe9adc105c42de3dea2b9b98f3
Lilian Daminelli Aguirre <lilidaminelli@gmail.com>
2024-07-18 12:35:01 +0200

    adding menu options - displaying users, habits, habits by user

--- HabitLogger/Program.cs
+++ HabitLogger/Program.cs
@@ -7,99 +7,119 @@ class Program
         string? readInputResult = "";
         string? menuSelection = "";
 
-        Console.Clear();
-
-        Console.WriteLine("Welcome to the Habit Logger App in C#");
-        Console.WriteLine("------------------------\n");
-
-        // Ask the user to choose an operation.
-        Console.WriteLine("Your main menu options are:");
-        Console.WriteLine("------------------------\n");
-
-        Console.WriteLine("1. To display the current database, type: 1");
-        Console.WriteLine("2. To display the current users, type: 2");
-        Console.WriteLine("3. To display the current habits, type: 3");
-        Console.WriteLine("4. To display the habits by selected user, type: 4");
-
-        Console.WriteLine("5. To create a new user and habit, type: 5");
-        // Console.WriteLine("6. To create a new user, type: 6");
-        // Console.WriteLine("7. To create a new habit, type: 7");
-
-        var currentDate = DateTime.Now;
-        Console.WriteLine($"{Environment.NewLine}Hello! on {currentDate:d} at {currentDate:t}");
-
-        Console.WriteLine("Enter your option (or type 0 to Exit the program)");
-        Console.WriteLine();
-
-        readInputResult = Console.ReadLine();
-
-        var acceptableMenuOption = "1 2 3 4 5".Split();
-
-        if (readInputResult != null)
+        while (menuSelection != "0")
         {
-            // validate for menu options
-            while (!acceptableMenuOption.Contains(readInputResult))
+            Console.Clear();
+
+            Console.WriteLine("Welcome to the Habit Logger App in C#");
+            Console.WriteLine("------------------------\n");
+
+            // Ask the user to choose an option.
+            Console.WriteLine("Your main menu options are:");
+            Console.WriteLine("------------------------\n");
+
+            Console.WriteLine("1. To display the current database, type: 1");
+            Console.WriteLine("2. To display the current users, type: 2");
+            Console.WriteLine("3. To display the current habits, type: 3");
+            Console.WriteLine("4. To display the habits by selected user, type: 4");
+
+            Console.WriteLine("5. To create a new user and habit, type: 5");
+            // Console.WriteLine("6. To create a new user, type: 6");
+            // Console.WriteLine("7. To create a new habit, type: 7");
+
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello! on {currentDate:d} at {currentDate:t}");
+
+            Console.WriteLine("Enter your option (or type 0 to Exit the program)");
+            Console.WriteLine();
+
+            readInputResult = Console.ReadLine();
+
+            var acceptableMenuOption = "1 2 3 4 5".Split();
+
+            if (readInputResult != null)
             {
-                Console.WriteLine("Enter your option (or type 0 to exit the program)");
-                Console.WriteLine();
-                readInputResult = Console.ReadLine();
+                // validate for menu options
+                while (!acceptableMenuOption.Contains(readInputResult))
+                {
+                    Console.WriteLine("Enter your option (or type 0 to exit the program)");
+                    Console.WriteLine();
+                    readInputResult = Console.ReadLine();
+                }
+
+                if (!String.IsNullOrEmpty(readInputResult))
+                {
+                    menuSelection = readInputResult.ToLower();
+                }
             }
 
-            if (!String.IsNullOrEmpty(readInputResult))
+            const string dataBaseFile = "consolehabits.db";
+
+            var connectionString = $"Data Source={dataBaseFile}";
+
+            using (var connection = new SqliteConnection(connectionString))
             {
-                menuSelection = readInputResult.ToLower();
+                connection.Open();
+
+                // C->Create
+                CreateHabitsTable(connection);
+
+                // call to CRUD Methods
+                switch (menuSelection)
+                {
+                    case "1": // database file
+                        DisplayAllTable(connection);
+
+                        Console.WriteLine("\n\rPress the Enter key to continue.");
+                        readInputResult = Console.ReadLine();
+                        break;
+
+                    case "2": // users
+                        DisplayAllUsers(connection);
+
+                        Console.WriteLine("\n\rPress the Enter key to continue.");
+                        readInputResult = Console.ReadLine();
+                        break;
+
+                    case "3": // habits
+                        DisplayAllHabits(connection);
+
+                        Console.WriteLine("\n\rPress the Enter key to continue.");
+                        readInputResult = Console.ReadLine();
+                        break;
+
+                    case "4": // display habits by selected user
+                        DisplayAllHabitsByUser(connection);
+
+                        Console.WriteLine("\n\rPress the Enter key to continue.");
+                        readInputResult = Console.ReadLine();
+                        break;
+
+                    case "5": // create new user and habit
+                        CreateNewUser(connection);
+
+                        Console.WriteLine("\n\rPress the Enter key to continue.");
+                        readInputResult = Console.ReadLine();
+                        break;
+
+                    default: // create new user and habit
+                        CreateNewUser(connection);
+
+                        Console.WriteLine("\n\rPress the Enter key to continue.");
+                        readInputResult = Console.ReadLine();
+                        break;
+
+                }
+
+                // U -> Update
+                // UpdateHabit(connection);
+                // UpdateAllHabits(connection);
+
+                // D -> Delete
+                // DeleteHabit(connection);
+                // DeleteAllHabits(connection);
+                // DisplayAllHabitsByUser(connection);
             }
-        }
-
-        const string dataBaseFile = "consolehabits.db";
-
-        var connectionString = $"Data Source={dataBaseFile}";
-
-        using (var connection = new SqliteConnection(connectionString))
-        {
-            connection.Open();
-
-            // C->Create
-            CreateHabitsTable(connection);
-
-            // call to CRUD Methods
-            switch (menuSelection)
-            {
-                case "1": // database file
-                    DisplayAllTable(connection);
-                    break;
-
-                case "2": // users
-                    DisplayAllUsers(connection);
-                    break;
-
-                case "3": // habits
-                    DisplayAllHabits(connection);
-                    break;
-
-                case "4": // display habits by selected user
-                    DisplayAllHabitsByUser(connection);
-                    break;
-
-                case "5": // create new user and habit
-                    CreateNewUser(connection);
-                    break;
-
-                default: // create new user and habit
-                    CreateNewUser(connection);
-                    break;
-
-            }
-
-
-            // U -> Update
-            // UpdateHabit(connection);
-            // UpdateAllHabits(connection);
-
-            // D -> Delete
-            // DeleteHabit(connection);
-            // DeleteAllHabits(connection);
-            // DisplayAllHabitsByUser(connection);
         }
 
         // // Clean up
@@ -113,10 +133,10 @@ class Program
 
             command.CommandText =
             @"
-            CREATE TABLE IF NOT EXISTS habits (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            name TEXT NULL,
-            habit TEXT);   
+                CREATE TABLE IF NOT EXISTS habits (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name TEXT NULL,
+                habit TEXT);   
             ";
 
             command.ExecuteNonQuery();
@@ -176,9 +196,9 @@ class Program
 
             command.CommandText =
             @"
-            SELECT *
-            FROM habits;
-        ";
+                SELECT *
+                FROM habits;
+            ";
 
             using (var reader = command.ExecuteReader())
             {
@@ -197,9 +217,9 @@ class Program
 
             command.CommandText =
             @"
-            SELECT habit
-            FROM habits;
-        ";
+                SELECT habit
+                FROM habits;
+            ";
 
             using (var reader = command.ExecuteReader())
             {
@@ -218,9 +238,9 @@ class Program
 
             command.CommandText =
             @"
-            SELECT id, name
-            FROM habits;
-        ";
+                SELECT name
+                FROM habits;
+            ";
 
             using (var reader = command.ExecuteReader())
             {
@@ -228,7 +248,7 @@ class Program
 
                 while (reader.Read())
                 {
-                    Console.WriteLine($"ID: {reader["id"]}, Name: {reader["name"]}");
+                    Console.WriteLine($"Name: {reader["name"]}");
                 }
             }
         }
@@ -247,9 +267,9 @@ class Program
 
                 command.CommandText =
                 @"
-                INSERT INTO habits (name, habit)
-                VALUES ($name, $habit);
-            ";
+                    INSERT INTO habits (name, habit)
+                    VALUES ($name, $habit);
+                ";
 
                 command.Parameters.AddWithValue("$name", name);
                 command.Parameters.AddWithValue("$habit", habit);
@@ -274,10 +294,10 @@ class Program
 
             command.CommandText =
             @"
-            SELECT habit
-            FROM habits
-            WHERE name = $name;
-        ";
+                SELECT habit
+                FROM habits
+                WHERE name = $name;
+            ";
 
             command.Parameters.AddWithValue("$name", name);
 
